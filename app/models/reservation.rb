0ab48a92a9b8ed23b608fd6b0509b6432e1d293d
Ayomipo Majek <ayomipomajek@gmail.com>
2023-07-21 20:38:29 +0100

    reservations model worked on

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,8 @@
 class Reservation < ApplicationRecord
   belongs_to :doctor
   belongs_to :patient
+
+  validates :city, presence: true
+  validates :date, presence: true
+  validates :time, presence: true
 end
