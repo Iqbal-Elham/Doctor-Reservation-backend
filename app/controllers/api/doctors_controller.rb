34c2d02baf542410356b09c0d879377d521b1354
Muhammad Davlatov <umedjon02.ud@gmail.com>
2023-07-25 00:38:13 +0500

    Update doctors_controller, added destroy method

--- app/controllers/api/doctors_controller.rb
+++ app/controllers/api/doctors_controller.rb
@@ -3,4 +3,10 @@ class Api::DoctorsController < ApplicationController
     @doctors = Doctor.all
     render json: @doctors
   end
+
+  def destroy
+    @doctor = Doctor.find(params[:id])
+    @doctor.destroy
+    render json: {message: 'Doctor deleted successfully'}
+  end
 end
