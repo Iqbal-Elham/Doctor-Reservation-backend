92728ed6e58686f55013d6b71d41107c7f9aac13
Muhammad Davlatov <umedjon02.ud@gmail.com>
2023-07-25 00:46:01 +0500

    Updaet routes.rb, used resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   namespace :api do
-    get 'doctors', to: 'doctors#index'
-    get 'doctor/delete', to: 'doctors#destroy'
+    resources :doctors, only: [:index, :destroy]
   end 
 end
