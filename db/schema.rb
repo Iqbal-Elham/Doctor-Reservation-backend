6f80914bef5082f72fa1e1fa41ecce9c60f92bdc
iqbal elham <iqbal.elham7@gmail.com>
2023-07-26 13:34:44 +0430

    Delete the password_digest column and add the full_name column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_25_163347) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_26_052816) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,9 +41,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_25_163347) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "full_name"
   end
 
   add_foreign_key "reservations", "doctors"
