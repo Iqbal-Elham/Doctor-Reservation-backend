eedc270f11f191ff7ea50e5fa1398ae296a54334
iqbal elham <iqbal.elham7@gmail.com>
2023-07-26 14:07:00 +0430

    Fix the linter errors'

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,17 @@
 class SessionsController < ApplicationController
-    def create
-        @user = User.find_by(username: params[:username])
-        if @user && @user.authenticate(params[:password])
-          token = generate_token(@user.id)
-          render json: { user: @user, token: token }
-        else
-          render json: { error: 'Invalid username or password' }, status: :unauthorized
-        end
-      end
-    
-      private
-    
-      def generate_token(user_id)
-        JWT.encode({ user_id: user_id }, Rails.application.credentials.secret_key_base)
-      end
+  def create
+    @user = User.find_by(username: params[:username])
+    if @user&.authenticate(params[:password])
+      token = generate_token(@user.id)
+      render json: { user: @user, token: }
+    else
+      render json: { error: 'Invalid username or password' }, status: :unauthorized
+    end
+  end
+
+  private
+
+  def generate_token(user_id)
+    JWT.encode({ user_id: }, Rails.application.credentials.secret_key_base)
+  end
 end
