066083bb90406f34c1a306a162ffeaac2747d67e
iqbal elham <iqbal.elham7@gmail.com>
2023-07-26 13:37:56 +0430

    Add user login and signup route in routes.rb file'

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
-  resources :users, only: [:create]
-  post '/login', to: 'sessions#create'
+  # resources :users, only: [:create]
+  # post '/login', to: 'sessions#create'
+
+  get '/users/:username', to: 'users#show'
+  post '/users/:username', to: 'users#show'
+  post '/users', to: 'users#create'
+  delete '/users/:username', to: 'users#destroy'
+  
 
   namespace :api do
+
+
     get 'doctors', to: 'doctors#index'
     resources :reservations, only: [:index]
   end 
