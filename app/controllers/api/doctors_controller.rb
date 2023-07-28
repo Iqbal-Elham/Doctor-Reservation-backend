adce74f36672111225d1352262a38a56808ebb61
Ayomipo Majek <ayomipomajek@gmail.com>
2023-07-28 17:23:21 +0100

    show method adjusted

--- app/controllers/api/doctors_controller.rb
+++ app/controllers/api/doctors_controller.rb
@@ -8,15 +8,6 @@ class Api::DoctorsController < ApplicationController
 
   def show
     @doctor = Doctor.find(params[:id])
-    @doctordata = {
-      id: @doctor.id,
-      name: @doctor.name,
-      about: @doctor.description,
-      photo: rails_blob_url(@doctor.photo),
-      price: @doctor.price,
-      user_id: @doctor.user_id,
-      username: @doctor.user.username
-    }
     render json: @doctor
   end
 
