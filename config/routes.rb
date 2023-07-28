35e8adc5a80cd2200ec3fe6d7cd9fd7cd64d6c9c
Muhammad Davlatov <umedjon02.ud@gmail.com>
2023-07-29 04:03:04 +0500

    Correct routes for the doctors controller

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
   
 
   namespace :api do
-    resources :doctors
     resources :doctors, only: [:index, :destroy]
     resources :reservations, only: [:index]
   end 
