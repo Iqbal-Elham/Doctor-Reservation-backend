70101238db3b2ac820008d1550e8d54922f2bc73
Ayomipo Majek <ayomipomajek@gmail.com>
2023-08-01 19:24:02 +0100

    username added to user model and reservations

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,17 +10,17 @@
 print "start"
 
 # Create some doctors
-doctor1 = Doctor.create(name: 'Dr. John Doe')
-doctor2 = Doctor.create(name: 'Dr. Jane Smith')
+doctor1 = Doctor.create(name: 'Dr. John Doe', photo: "wwwwww",price_hour: 20, about: "what")
+doctor2 = Doctor.create(name: 'Dr. Jane Smith', photo: "xxxxxxx", price_hour: 30, about: "whatever")
 
-# Create some patients
-patient1 = Patient.create(name: 'Alice')
-patient2 = Patient.create(name: 'Bob')
+# # Create some patients
+# patient1 = Patient.create(name: 'Alice')
+# patient2 = Patient.create(name: 'Bob')
 
-# Create some reservations with appointment_time
-Reservation.create(patient_id: patient1.id, doctor_id: doctor1.id, city: 'New York', appointment_time: Time.now + 1.day)
-Reservation.create(patient_id: patient1.id, doctor_id: doctor2.id, city: 'Los Angeles', appointment_time: Time.now + 2.days)
-Reservation.create(patient_id: patient2.id, doctor_id: doctor1.id, city: 'Chicago', appointment_time: Time.now + 3.days)
-Reservation.create(patient_id: patient2.id, doctor_id: doctor2.id, city: 'San Francisco', appointment_time: Time.now + 4.days)
+# # Create some reservations with appointment_time
+# Reservation.create(patient_id: patient1.id, doctor_id: doctor1.id, city: 'New York', appointment_time: Time.now + 1.day)
+# Reservation.create(patient_id: patient1.id, doctor_id: doctor2.id, city: 'Los Angeles', appointment_time: Time.now + 2.days)
+# Reservation.create(patient_id: patient2.id, doctor_id: doctor1.id, city: 'Chicago', appointment_time: Time.now + 3.days)
+# Reservation.create(patient_id: patient2.id, doctor_id: doctor2.id, city: 'San Francisco', appointment_time: Time.now + 4.days)
 
 print "end"
