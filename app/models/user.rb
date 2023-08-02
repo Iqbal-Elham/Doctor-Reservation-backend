bdffd423f4ac13774fdb1529c3564b04e4a2574a
Muhammad Davlatov <umedjon02.ud@gmail.com>
2023-08-03 02:47:07 +0500

    Update user.rb, remove the comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_many :reservations, dependent: :destroy
   has_many :doctors, through: :reservations, dependent: :destroy
-  # validates :username, uniqueness: { case_sensitive: false }
+  validates :username, uniqueness: { case_sensitive: false }
 end
